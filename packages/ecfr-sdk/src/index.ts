5638f286a5aeb848dae8eec5298f1021ec5b00d8
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2025-08-03 22:07:51 +0300

    fix: add missing API function exports to ecfr-sdk and govinfo-sdk

--- packages/ecfr-sdk/src/index.ts
+++ packages/ecfr-sdk/src/index.ts
@@ -3,5 +3,8 @@ import * as packageJson from '../package.json';
 // Export the configured API client
 export * from './api/client';
 
+// Export all generated API functions
+export * from './api/generated/endpoints';
+
 // Export package version
 export const VERSION = packageJson.version;
